c58804f095fc23d2e5222f6f48df2c70bc71bb25
Block Doc <matthew.stammers@uhs.nhs.uk>
2022-09-03 12:16:25 +0100

    basic game is complete

--- src/views.py
+++ src/views.py
@@ -11,7 +11,7 @@ class MenuView(arcade.View):
                          arcade.color.BLACK, font_size=50, anchor_x="center")
         arcade.draw_text("A crazy physics platformer", self.window.width / 2, self.window.height / 2 - 75,
                          arcade.color.RED_BROWN, font_size=25, anchor_x="center")
-        arcade.draw_text("Click to advance", self.window.width / 2, self.window.height / 2 - 150,
+        arcade.draw_text("Click to see the instructions", self.window.width / 2, self.window.height / 2 - 150,
                          arcade.color.REDWOOD, font_size=20, anchor_x="center")
 
     def on_mouse_press(self, _x, _y, _button, _modifiers):
@@ -31,15 +31,15 @@ class InstructionView(arcade.View):
     def on_draw(self):
         """ Draw this view """
         self.clear()
-        arcade.draw_text("Instructions Screen", self.window.width / 2, self.window.height / 2,
+        arcade.draw_text("Game Instructions", self.window.width / 2, self.window.height / 2,
                          arcade.color.WHITE, font_size=50, anchor_x="center")
         arcade.draw_text("ASDW or arrows to move, Q/N for bullets", self.window.width / 2, self.window.height / 2-80,
                          arcade.color.PINK_LAVENDER, font_size=25, anchor_x="center")
-        arcade.draw_text("E/M to shield, Click for grenades which level up as you do", self.window.width / 2, self.window.height / 2-120,
+        arcade.draw_text("E/M to shield, Mouse for grenades. Points increase your level", self.window.width / 2, self.window.height / 2-120,
                          arcade.color.YELLOW_GREEN, font_size=25, anchor_x="center")
         arcade.draw_text("Increasing your level improves bullets, damage and jump", self.window.width / 2, self.window.height / 2-160,
                          arcade.color.DARK_CHESTNUT, font_size=25, anchor_x="center")
-        arcade.draw_text("Grenades activate at level one and become increasingly powerful", self.window.width / 2, self.window.height / 2-200,
+        arcade.draw_text("Grenades activate at level one. Keep an eye out for power ups", self.window.width / 2, self.window.height / 2-200,
                          arcade.color.PINK_LAVENDER, font_size=25, anchor_x="center")
 
     def on_mouse_press(self, _x, _y, _button, _modifiers):
